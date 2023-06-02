8818df2ecd7ce289ed697e9e0b28a2514466b192
David W. Gray <dwgray67@hotmail.com>
2023-06-02 13:58:19 -0700

    Some cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,6 @@ class Counter with ChangeNotifier {
   }
 
   void onTimeout() {
-    log('Entering Timeout: $_clickState');
     switch (_clickState) {
       case ClickState.initial:
       case ClickState.firstClick:
@@ -129,89 +128,58 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Beat Counter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Beat Counter'),
+      home: Consumer<Counter>(
+          builder: (context, state, child) =>
+              MyHomePage(title: 'Beat Counter', state: state)),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({super.key, required this.title, required this.state});
   final String title;
+  final Counter state;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    log('${state.method}');
+    var mpm = state.method == CountMethod.measure
+        ? <Widget>[
+            Text("${state.mpm.toStringAsFixed(1)} mpm ${state.meter.index}/4"),
+            const Spacer(flex: 1),
+          ]
+        : [];
+
+    var elements = <Widget>[
+      const Spacer(flex: 2),
+      ElevatedButton(
+        onPressed: () {
+          state.click();
+        },
+        child: Text(state.clickLabel),
+      ),
+      const Spacer(flex: 1),
+      ...mpm,
+      Text("${state.bpm.toStringAsFixed(1)} bpm"),
+      const Spacer(flex: 2),
+      const MeterChooser(),
+      const Spacer(flex: 1),
+      const MethodChooser(),
+      const Spacer(flex: 1),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Center(child: Text(title)),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Spacer(flex: 2),
-            ElevatedButton(
-              onPressed: () {
-                var state = context.read<Counter>();
-                state.click();
-              },
-              child: Consumer<Counter>(
-                  builder: (context, state, child) => Text(state.clickLabel)),
-            ),
-            const Spacer(flex: 1),
-            Consumer<Counter>(
-                builder: (context, state, child) => Text(
-                    "${state.mpm.toStringAsFixed(1)} mpm ${state.meter.index}/4")),
-            const Spacer(flex: 1),
-            Consumer<Counter>(
-                builder: (context, state, child) =>
-                    Text("${state.bpm.toStringAsFixed(1)} bpm")),
-            const Spacer(flex: 2),
-            const MeterChooser(),
-            const Spacer(flex: 1),
-            const MethodChooser(),
-            const Spacer(flex: 1),
-          ],
+          children: elements,
         ),
       ),
     );
@@ -228,7 +196,6 @@ class MeterChooser extends StatelessWidget {
     return Consumer<Counter>(
         builder: (context, state, child) => SegmentedButton<Meter>(
                 segments: const <ButtonSegment<Meter>>[
-                  ButtonSegment<Meter>(value: Meter.beat, label: Text('beat')),
                   ButtonSegment<Meter>(value: Meter.double, label: Text('2/4')),
                   ButtonSegment<Meter>(value: Meter.waltz, label: Text('3/4')),
                   ButtonSegment<Meter>(value: Meter.common, label: Text('4/4')),
